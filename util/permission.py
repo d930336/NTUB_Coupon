6a7c9da6941a068498ffe72152167f7ca3b1e38e
jun_yj_chang <jun_yj_chang@gss.com.tw>
2019-12-09 17:56:43 +0800

    驗證

---
+++ util/permission.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        #obj可以看成資料庫裡model，這裡把owner改成我們資料库中的user
+        return obj.user == request.user
